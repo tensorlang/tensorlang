4e254c988b39ee2dc44646d31725be6fcb077297
Adam Bouhenguel <adam@bouhenguel.com>
2017-02-05 18:36:46 -0500

    Remove cruft in mnist_data.py

--- scratch/mnist_data.py
+++ scratch/mnist_data.py
@@ -15,10 +15,6 @@
 
 """Functions for reading MNIST data."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import gzip
 import numpy
 
